25d4fa456a47f07fdfaf653e1cfd895191e37841
Arran Dyer <arran549@gmail.com>
2018-06-24 23:58:42 +0100

    feat: display using deck card

--- components/ViewDecks.js
+++ components/ViewDecks.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux'
+import DeckCard from './DeckCard'
+import Title from './Title'
 
 class ViewDecks extends Component {
     render () {
+
+        const { navigation } = this.props
+
         return (
+           
             <View>
+                <Title title="Decks" />
                 {
-                    this.props.decks.map(deck => (<Text key={deck.name} >{deck.name}</Text>))
+                    this.props.decks.map(deck => (<DeckCard key={deck.id} deck={deck} navigation={navigation}></DeckCard>))
                 }
             </View>
         )
