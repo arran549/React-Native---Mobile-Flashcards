8c7b9ede49c4df404861ecb8272ca29b982a7a1e
Arran Dyer <arran549@gmail.com>
2018-06-24 13:05:51 +0100

    feat: add skeleton AddCardView component

---
+++ components/AddCardView.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import TextButton from './TextButton'
+import { connect } from 'react-redux'
+
+class AddCardView extends Component {
+
+}
+
+function mapStateToProps (state) {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(AddCardView)
