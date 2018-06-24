a908f393a4f91f5091666031234383269780a907
Arran Dyer <arran549@gmail.com>
2018-06-24 12:56:38 +0100

    feat: add card to deck action

--- actions/index.js
+++ actions/index.js
@@ -2,3 +2,10 @@ export const CREATE_NEW_DECK = 'CREATE_NEW_DECK'
 export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK'
 export const RECIEVE_DECKS = 'RECEIVE_DECKS'
 
+export function addCardToDeck (card, deckId) {
+    return {
+        type: ADD_CARD_TO_DECK,
+        card,
+        deckId
+    }
+}
