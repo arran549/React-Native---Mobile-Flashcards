cab1280e082043d4d046ccfaf5ccc72ef3040a9d
Arran Dyer <arran549@gmail.com>
2018-06-24 12:28:42 +0100

    refactor: move to actions/index.js

---
+++ actions/index.js
@@ -0,0 +1,4 @@
+export const CREATE_NEW_DECK = 'CREATE_NEW_DECK'
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK'
+export const RECIEVE_DECKS = 'RECEIVE_DECKS'
+
--- reducers/index.js
+++
@@ -1,3 +0,0 @@
-export const CREATE_NEW_DECK = 'CREATE_NEW_DECK'
-export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK'
-export const RECIEVE_DECKS = 'RECEIVE_DECKS'
