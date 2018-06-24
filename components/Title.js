0fed99e6e7293a7f527bdb0ad7fa6f876c7d3da3
Arran Dyer <arran549@gmail.com>
2018-06-24 23:58:11 +0100

    feat: add title component

---
+++ components/Title.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { purple } from '../utils/colors'
+
+class Title extends Component {
+    render () {
+
+        const { title } = this.props
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        )
+    }
+}
+
+export default Title
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 30,
+        color: purple,
+    },
+    container: {
+        // flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    }
+})
+
