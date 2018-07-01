1976dd6ad7a76d210ec684b72c165e7bd0eb4fdc
Arran Dyer <arran549@gmail.com>
2018-07-01 13:58:15 +0100

    feat: style add card to deck

--- components/AddCardView.js
+++ components/AddCardView.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Platform } from 'react-native';
 import TextButton from './TextButton'
 import { connect } from 'react-redux'
 import { addCardToDeck } from '../actions'
 import { saveCardToDeck } from '../utils/api'
+import { gray, white } from '../utils/colors'
 
 class AddCardView extends Component {
 
     state = {
-        question: 'When was udacity founded?',
-        answer: 'something'
+        question: null,
+        answer: null
     }
 
     addCard = () => {
@@ -26,6 +27,13 @@ class AddCardView extends Component {
         saveCardToDeck(deckId, card).then( () => {
             this.props.addCard(card, deckId)
         })
+
+        this.setState({
+            question: null,
+            answer: null
+        })
+
+        
     }
 
     render () {
@@ -33,18 +41,18 @@ class AddCardView extends Component {
         
 
         return (
-            <View>
-                <Text>Add a card</Text>
-                <Text>DeckId: {this.props.navigation.state.params.deckId}</Text>
-                <Text>Question</Text>
-                <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                            onChangeText={(text)=> this.setState({ question: text })} value={this.state.question} />
+            <View style={styles.container}>
+                <Text style={{fontSize: 20, marginTop: 20}}>Add a card</Text>
+                
+                <Text style={styles.label}>Question</Text>
+                <TextInput  style={styles.input}
+                            onChangeText={(text)=> this.setState({ question: text })} value={this.state.question} placeholder='Question' />
 
-                <Text>Answer</Text>
-                <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                            onChangeText={(text)=> this.setState({ answer: text })} value={this.state.answer} />
+                <Text style={styles.label}>Answer</Text>
+                <TextInput  style={styles.input}
+                            onChangeText={(text)=> this.setState({ answer: text })} value={this.state.answer} placeholder='Answer' />
 
-                <TextButton onPress={this.addCard}>SUBMIT</TextButton>
+                <TextButton style={styles.button} onPress={this.addCard}>SUBMIT</TextButton>
             </View>
         )
     }
@@ -63,3 +71,39 @@ function mapDispatchToProps (dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddCardView)
+
+const styles = StyleSheet.create({
+    label: {
+        marginTop: 20
+    },
+    button: {
+        marginTop: 20
+    },
+    input: {
+        height: 40, 
+        width: 300, 
+        borderColor: gray, 
+        borderWidth: 1, 
+        marginTop: 10,
+        paddingLeft: 10,
+        borderRadius: 10
+    },
+    container: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        height: 300,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        }
+    }
+})
